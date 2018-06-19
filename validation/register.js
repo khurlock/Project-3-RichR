ee6d8384165a2867c3667245bbcb85eea0477252
Jacobv2410 <jacobv2410@gmail.com>
2018-06-18 22:39:11 -0700

    continued on validation and login

--- validation/register.js
+++ validation/register.js
@@ -13,7 +13,7 @@ module.exports = function validateRegisterInput(data) {
     errors.name = "Name must be between 2 and 30 characters";
   }
 
-  if (Validator.isEmpty(data.name)) {
+  if (!Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
 
@@ -21,11 +21,15 @@ module.exports = function validateRegisterInput(data) {
     errors.email = "Email field is required";
   }
 
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
 
-  if (Validator.isLength(data.password, { min: 6, max: 30 })) {
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters";
   }
 
@@ -33,7 +37,7 @@ module.exports = function validateRegisterInput(data) {
     errors.password2 = "Confirm Password field is required";
   }
 
-  if (Validator.equals(data.password, data.password2)) {
+  if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords must match";
   }
 

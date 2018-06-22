a342ea3ee42e573d3e70c1a78dc64287c9951bf7
Jacobv2410 <jacobv2410@gmail.com>
2018-06-21 22:26:36 -0700

    home landing page styled

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">RichR</h1>
+                <p className="lead">
+                  {" "}
+                  At the end of this year, will you be richer?
+                  <br />
+                  Find out now...
+                </p>
+                <hr />
+                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                  Get RichR
+                  {/* </a>
+                {/* <a href="login.html" className="btn btn-lg btn-light">
+                  Login */}
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;

c458f12a9e7c20585caca17e8a3ead40ce3c790a
Dev533 <jb.developer.fs@gmail.com>
2018-06-25 18:11:40 -0700

    update

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ app.use("/api/home", home);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`http://localhost:${port}`));

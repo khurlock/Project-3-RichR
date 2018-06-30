984ec77ec4ea70c35effd70402b5744ba6c99886
Dev533 <jb.developer.fs@gmail.com>
2018-06-29 20:35:42 -0700

    hero to the rescue

--- server.js
+++ server.js
@@ -49,7 +49,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//my ports
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
